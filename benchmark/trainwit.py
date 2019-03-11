fc26851f90783fc739acdbea8674fe2a2eb964ee
Anda Stoica <andadiana.stoica@gmail.com>
2019-03-11 11:15:56 +0200

    Converting SnipsNLU format to Wit.ai format

--- benchmark/trainwit.py
+++ benchmark/trainwit.py
@@ -3,30 +3,71 @@ import requests
 
 TOKEN = ''
 url = 'https://api.wit.ai/entities?v=20170307'  # TODO: dynamic date
-data = {'doc': 'A city that I like',
-       'id': 'favorite_city'}    # Set POST fields here
+entities1 = {'doc': 'Name',
+       'id': 'entity_name'}
 headers = {
     'Authorization': 'Bearer ' + TOKEN,
     'Content-Type': 'application/json'
 }
-# r = requests.post(url, data=json.dumps(data), headers=headers)
+# r = requests.post(url, data=json.dumps(entities1), headers=headers)
 # print(r.content)
 
 url_samples = 'https://api.wit.ai/samples?v=20170307'  # TODO: dynamic date
-sample_data = [{
-        "text": "I like San Francisco",
+sample_data2 = [
+    {
+        "text": "add Stani, stani Ibar vodo songs in my playlist música libre",
         "entities": [
-          {
-            "entity": "intent",
-            "value": "stating_fact"
-          },
-          {
-            "entity": "favorite_city",
-            "start": 7,
-            "end": 19,
-            "value": "San Francisco"
-          }
+            {
+                "entity": "intent",
+                "value": "AddToPlaylist"
+            },
+            {
+                "start": 4,
+                "end": 26,
+                "value": "Stani, stani Ibar vodo",
+                "entity": "entity_name"
+            },
+            {
+                "start": 36,
+                "end": 38,
+                "value": "my",
+                "entity": "playlist_owner"
+            },
+            {
+                "start": 48,
+                "end": 61,
+                "value": "música libre",
+                "entity": "playlist"
+            }
         ]
-      }]
-r = requests.post(url_samples, data=json.dumps(sample_data), headers=headers)
+    },
+    {
+        "text": "add this album to my Blues playlist",
+        "entities": [
+            {
+                "entity": "intent",
+                "value": "AddToPlaylist"
+            },
+            {
+                "start": 9,
+                "end": 14,
+                "value": "album",
+                "entity": "music_item"
+            },
+            {
+                "start": 18,
+                "end": 20,
+                "value": "my",
+                "entity": "playlist_owner"
+            },
+            {
+                "start": 21,
+                "end": 26,
+                "value": "Blues",
+                "entity": "playlist"
+            }
+        ]
+    }
+    ]
+r = requests.post(url_samples, data=json.dumps(sample_data2), headers=headers)
 print(r.content)
