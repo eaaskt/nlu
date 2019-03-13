bc73c191f052d8edb4255ed19237b7c3b6772297
kadartibor <kadartibor24@gmail.com>
2019-03-13 21:45:14 +0200

    adds script for getting wit response for validation set

---
+++ benchmark/verify_samples.py
@@ -0,0 +1,71 @@
+import json
+import requests
+import argparse
+import configparser
+import datetime
+
+url = ''
+TOKEN = ''
+
+
+def read_config_file(config_file, app_name):
+    global url
+    global TOKEN
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    url = config['URL']['get_message']
+    if app_name == 'snips':
+        TOKEN = config['TOKEN']['snips_token']
+    else:
+        TOKEN = config['TOKEN']['atis_token']
+
+
+def get_messages(input_file, output_file, failed_file):
+    global url
+    now = datetime.datetime.now()
+    url = url + str(now.year) + '{num:02d}'.format(num=now.month) + '{num:02d}'.format(num=now.day) + '&q='
+    print(url)
+    print(TOKEN)
+    headers = {
+        'Authorization': 'Bearer ' + TOKEN,
+        'Content-Type': 'application/json'
+    }
+
+    with open(input_file) as inf:
+        json_data = json.loads(inf.read())
+
+    failed_list = []
+    response = []
+    for sample in json_data:
+        sample_url = url + sample['text']
+        try:
+            r = requests.get(sample_url, headers=headers)
+            print(r.content)
+            if r.status_code != 200:
+                failed_list.append(sample)
+            else:
+                response.append(r.content)
+
+        except Exception as e:
+            print(e)
+
+    with open(failed_file, "w") as ff:
+        json.dump(failed_list, ff)
+
+    with open(output_file, "w") as of:
+        json.dump(response, of)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+                description="Get results from Wit.ai for validation set",
+                usage="post_entities.py <config_file> <input_file> <failed_samples_file> <app_name>")
+    parser.add_argument('config_file', help='Config file')
+    parser.add_argument('input_file', help='Input file')
+    parser.add_argument('output_file', help='Output file')
+    parser.add_argument('failed_file', help='failed file')
+    parser.add_argument('app', help='App to post data to')
+    args = parser.parse_args()
+    read_config_file(args.config_file, args.app)
+    get_messages(input_file=args.input_file, failed_file=args.failed_file, output_file=args.output_file)
+
