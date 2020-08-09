ee07561c91ae66bd969b556d25e4b0d3193d210f
Anda Stoica <andadiana.stoica@gmail.com>
2020-08-09 15:40:54 +0300

    Faster loading for word vectors

--- capsnet-arch/data_loader.py
+++ capsnet-arch/data_loader.py
@@ -1,6 +1,6 @@
 import numpy as np
 import util
-from gensim.models.keyedvectors import KeyedVectors
+from gensim.models import FastText
 import time
 
 
@@ -13,10 +13,10 @@ def load_w2v(file_name):
     """
 
     start = time.time()
-    w2v = KeyedVectors.load_word2vec_format(file_name, binary=False)
+    w2v = FastText.load_fasttext_format(file_name)
     end = time.time()
     print("loading time took %06.2f" % (start - end))
-    return w2v
+    return w2v.wv
 
 
 def load_vec(file_path, w2v, in_max_len, intent_dict, intent_id, slot_dict, slot_id, load_text=False):
